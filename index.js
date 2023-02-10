dbec6db89c7f03f0a79a215e5ee8d4989364382c
Karim Roy <pgs.karimroy@gmail.com>
2023-02-10 08:55:25 +0700

    Update

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 
-const io = new Server({
-  cors: {
-    origin: ["http://172.19.196.203", "http://localhost", "http://localhost:3000", "http://172.19.196.203:8080"],
-    credentials: true,
-  },
+// const io = new Server({
+//   cors: {
+//     origin: ["http://172.19.196.203", "http://localhost", "http://localhost:3000", "http://172.19.196.203:8080"],
+//     credentials: true,
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
+//   socket.on("chat message", (msg) => {
+//     io.emit("chat message", msg);
+//   });
+// });
+
+// io.listen(process.env.PORT || 3000);
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
-  });
+server.listen(3000, () => {
+  console.log("listening on *:3000");
 });
-
-io.listen(process.env.PORT || 3000);
