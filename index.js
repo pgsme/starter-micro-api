77f35f3c396aafa025661556e26ad532f16071ee
Karim Roy <pgs.karimroy@gmail.com>
2023-02-10 08:46:06 +0700

    Update Commit

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const { Server } = require("socket.io");
+
+const io = new Server({
+  cors: {
+    origin: ["http://172.19.196.203", "http://localhost", "http://localhost:3000"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+io.listen(process.env.PORT || 3000);
